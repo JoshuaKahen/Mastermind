765bbab0beadfd8286edbc7b6a0636468aeb09e0
JoshuaKahen <67845165+JoshuaKahen@users.noreply.github.com>
2022-06-21 13:10:20 -0400

    Updated Mastermind.py
    Changed structure of for-loops and improved time complexity

--- MasterMind.py
+++ MasterMind.py
@@ -1,3 +1,5 @@
+# Test cases
+
 
 code1 = ['F', 'A', 'I', 'R']
 guess1 = ['F', 'R', 'R', 'F']
@@ -15,10 +17,6 @@ code5 = ['A', 'F', 'F', 'F']
 guess5 = ['F', 'A', 'A', 'A']
 
 
-# Best case Time Complexity: O(n) since the definition is in a for loop and the best case is that it is a black pin
-# Worst case Time Complexity: O(n^3) since the definition has the potential of going through a loop within a dictionary
-# and then within another loop
-
 def mastermind(code, guess):
     black = 0
     white = 0
@@ -31,22 +29,19 @@ def mastermind(code, guess):
         else:
             counter[code[i]] = [i]
 
-    # This for-loop simply checks for black pins by comparing the two spots at the same time
+    # This for-loop checks for black pins by comparing the two spots at the same time
+    # This for-loop then removes those pins from the index of the array in the hashmap/dictionary
     for i in range(len(code)):
         if guess[i] == code[i]:
             black = black + 1
+            counter.get(code[i]).remove(i)
 
-        # Because this is using an elif statement, it will not count any of the white pins alongside a black pin
-        # This checks if there are any white pins by looking into the dictionary for the current digit
-        # If the current digit exists in the dictionary then it will use that digit's array from the dictionary and
-        # start to compare the digits of the code and the guess based on the indexes from the array
-        # in order to make sure the program does not count any black pins as white pins if the current array's
-        # index for both the code and the guess match
-        elif counter.get(guess[i]) is not None:
-            spare = counter.get(guess[i])
-            for j in spare:
-                if code[j] != guess[j]:
-                    white = white + 1
+    # This for-loop checks for white pins by checking if the digit is in the hashmap/dictionary and making sure
+    # that the current index in the guess and code are not the same
+    # It then adds the length of the array from the hashmap/dictionary based on the digit to the overall count
+    for i in range(len(code)):
+        if (counter.get(guess[i]) is not None) and (guess[i] != code[i]):
+            white = white + len(counter.get(guess[i]))
 
     return black, white
 
