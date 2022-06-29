f5a3fbfa37255a935a2f12cae738afb02c231b1d
JoshuaKahen <67845165+JoshuaKahen@users.noreply.github.com>
2022-06-29 10:18:27 -0400

    Changed Test Format
    Changed format in order to insert custom test cases

--- MasterMind.py
+++ MasterMind.py
@@ -1,22 +1,3 @@
-# Test cases
-
-
-code1 = ['F', 'A', 'I', 'R']
-guess1 = ['F', 'R', 'R', 'F']
-
-code2 = ['F', 'A', 'I', 'R']
-guess2 = ['R', 'I', 'A', 'F']
-
-code3 = ['A', 'F', 'I', 'F']
-guess3 = ['F', 'A', 'A', 'F']
-
-code4 = ['F', 'F', 'F', 'F']
-guess4 = ['F', 'A', 'A', 'A']
-
-code5 = ['A', 'F', 'F', 'F']
-guess5 = ['F', 'A', 'A', 'A']
-
-
 def mastermind(code, guess):
     black = 0
     white = 0
@@ -46,13 +27,16 @@ def mastermind(code, guess):
     return black, white
 
 
-print(mastermind(code1, guess1))
-# (1, 2)
-print(mastermind(code2, guess2))
-# (0, 4)
-print(mastermind(code3, guess3))
-# (1, 3)
-print(mastermind(code4, guess4))
-# (1, 0)
-print(mastermind(code5, guess5))
-# (0, 6)
+num = int(input("What is the amount of digits in the code?\n"))
+inpcode = []
+inpguess = []
+for i in range(num):
+    inp = input("Please enter a single digit for spot #" + str(i + 1) + " in the secret code\n")
+    inpcode.append(inp)
+
+for i in range(num):
+    inp = input("Please enter a single digit for spot #" + str(i + 1) + " in the guess\n")
+    inpguess.append(inp)
+
+print(mastermind(inpcode, inpguess))
+
